66ae684e2a71e8b0810d28a5faedac0426ac807d
Salah-Eddine Amsaafi <samsaafi@c4r8p8.1337.ma>
2023-11-01 20:05:38 +0100

    m3tasploit

--- libft.h
+++ libft.h
@@ -12,6 +12,9 @@ void *ft_memset (void *prt, int value, size_t num);
 void ft_bzero(void *s, size_t n);
 void *ft_memcpy (void *dest, const void *src, size_t n);
 void *ft_memmove(void *dest, const void *src, size_t n);
+size_t	ft_strlcpy(char *dest,const char *src, size_t size);
 size_t  ft_strlcat(char *dest,const char *src, size_t size);
+int ft_toupper(int c);
+int ft_tolower(int c);
 
 #endif
