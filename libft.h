ec10f15c44f27966d9abab1fc1f5737f6b4a1974
Salah-Eddine Amsaafi <samsaafi@c4r8p8.1337.ma>
2023-11-01 21:51:03 +0100

    add a function

--- libft.h
+++ libft.h
@@ -17,5 +17,6 @@ size_t  ft_strlcat(char *dest,const char *src, size_t size);
 int ft_toupper(int c);
 int ft_tolower(int c);
 char *ft_strchr(const char *str, int c);
+char *ft_strrchr(const char *str, int c);
 
 #endif
