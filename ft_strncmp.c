1df2773ec26d6a5a2593082f65a35c9f7128aee6
Salah-Eddine Amsaafi <samsaafi@e2r12p12.1337.ma>
2023-11-05 16:19:38 +0100

    fixed

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -20,7 +20,7 @@ int	ft_strncmp(const char *s1, char *s2, size_t n)
 	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
 	{
 		if (s1[i] > s2[i] || s1[i] < s2[i])
-			return (s1[i] - s2[i]);
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
 	return (0);
