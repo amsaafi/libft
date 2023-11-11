b3c22baa8be5414949fceb6339caaa1d3aa31174
Salah-Eddine Amsaafi <samsaafi@e2r13p10.1337.ma>
2023-11-11 22:45:52 +0100

    lstadd_back

--- Makefile
+++ Makefile
@@ -42,6 +42,7 @@ BSRC = ft_lstnew.c \
 		ft_lstadd_front.c \
 		ft_lstsize.c \
 		ft_lstlast.c \
+		ft_lstadd_back.c \
 
 
 OBJ = $(SRC:.c=.o)
---
+++ ft_lstadd_back.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: samsaafi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/11 21:33:48 by samsaafi          #+#    #+#             */
+/*   Updated: 2023/11/11 21:33:52 by samsaafi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*ptr;
+
+	if (!new)
+		return ;
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	ptr = ft_lstlast(*lst);
+	ptr->next = new;
+}
